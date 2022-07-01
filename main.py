59cdab5a3aa1edf938d82c3f1a3866fd3f8c295e
AnimatronicEmulator <leslie.durkey@gmail.com>
2022-07-01 18:51:29 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+register = MoneyMachine()
+menu = Menu()
+coffee_machine = CoffeeMaker()
+
+prompt = 'on'
+while prompt != 'off':
+    prompt = input(f"What would you like? {menu.get_items()}: ").lower()
+
+    if prompt == "report":
+        coffee_machine.report()
+        register.report()
+    elif prompt != "off":
+        drink = menu.find_drink(prompt)
+        if coffee_machine.is_resource_sufficient(drink) and register.make_payment(drink.cost):
+            coffee_machine.make_coffee(drink)
